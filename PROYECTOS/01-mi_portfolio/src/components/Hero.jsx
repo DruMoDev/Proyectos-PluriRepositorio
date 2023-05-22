34d86cf877f46c6c448bf3c4afdfa5ae602bd455
DruMoDev <dev.morera@gmail.com>
2023-05-22 17:45:08 +0200

    img

--- PROYECTOS/01-mi_portfolio/src/components/Hero.jsx
+++ PROYECTOS/01-mi_portfolio/src/components/Hero.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import imagen1 from "../assets/yo.png";
 
 const Hero = () => {
   return (
-    <div id="hero" className=' w-10/12 flex flex-col items-center justify-center gap-10 lg:gap-20 mx-auto'>
+    <div
+      id='hero'
+      className=' w-10/12 flex flex-col items-center justify-center gap-10 lg:gap-20 mx-auto'
+    >
       <div>
         <h1 className='leading-10 md:text-3xl lg:text-[33px] text-primary text-2xl hover:text-secondary text-center'>
           <span className='text-dru_white'>My name is </span>Dru
@@ -12,7 +16,7 @@ const Hero = () => {
       </div>
 
       <div className='rounded-full overflow-hidden border-4 border-primary h-[250px] w-[250px] flex justify-center items-center lg:h-[400px] lg:w-[400px]'>
-        <img src='/public/yo.png'/>
+        <img src={imagen1} />
       </div>
     </div>
   );
